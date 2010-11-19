decc25d289b6e7a852952aafdeaf993ea375d661
Aleksey Radchenko <dixon.che@gmail.com>
2010-11-19 12:19:58 +0200

    pep8 styling

--- slatino/Articles/views.py
+++ slatino/Articles/views.py
@@ -9,13 +9,15 @@ from dcdjutils.Articles.models import Article, ArticlePhoto
 article_last_count = settings.ARTICLE_LAST_COUNT or 5
 article_per_page = settings.ARTICLE_PER_PAGE or 10
 
+
 def article_last(request):
-    articles = Article.objects.filter(published=True).order_by('order').order_by('-pub_date')[:article_last_count]
+    articles = Article.objects.filter(published=True, post_type='article').order_by('order').order_by('-pub_date')[:article_last_count]
     return render_to_response("Articles/list.html", RequestContext(request,
                               dict(articles=articles, title=_(u"Last Articles"))))
 
+
 def article_list(request):
-    articles = Article.objects.filter(published=True).order_by('order').order_by('-pub_date')
+    articles = Article.objects.filter(published=True, post_type='article').order_by('order').order_by('-pub_date')
     page = request.GET.get('page', None)
 
     if page is not None:
@@ -26,20 +28,22 @@ def article_list(request):
         articles = articles[page*article_per_page:page*article_per_page+article_per_page]
     else:
         articles = articles[:article_per_page]
-        
+
     if not articles:
         return Http404()
-        
+
     return render_to_response("Articles/list.html", RequestContext(request,
                               dict(articles=articles, title=_(u"List Articles"))))
-                              
+
+
 def article_view(request, article_slug):
-    article = get_object_or_404(Article, slug=article_slug)
+    article = get_object_or_404(Article, slug=article_slug, post_type='article')
     return render_to_response("Articles/one.html", RequestContext(request,
                               dict(article=article)))
 
+
 def past_image(request, article_id):
-    article = get_object_or_404(Article, id=article_id)
+    article = get_object_or_404(Article, id=article_id, post_type='article')
     return render_to_response("Articles/past_image.html", RequestContext(request, dict(article=article)))
 
 
@@ -48,5 +52,5 @@ def image_view(request, image_id, size=None):
 
     if size is None:
         return  HttpResponseRedirect(image.image.url)
-  
+
     return  HttpResponseRedirect(image.thumbnail_url(size))
