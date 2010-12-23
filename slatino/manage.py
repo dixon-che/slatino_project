1c7ad581eb18dc3628c67dc5667884b46d55a66e
Aleksey Radchenko <dixon.che@gmail.com>
2010-12-23 14:27:53 +0200

    print removed

--- slatino/manage.py
+++ slatino/manage.py
@@ -8,7 +8,6 @@ try:
     if settings.ROOT_PATH[:-1] in sys.path:
         sys.path.remove(settings.ROOT_PATH[:-1])
 
-    print sys.path
 except ImportError:
     sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
     sys.exit(1)
