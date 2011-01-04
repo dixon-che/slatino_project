4cb04b596e0b660a8e6c650b6bf703785520c053
Lextor <lextor@ukr.net>
2011-01-04 21:25:55 +0200

    add date of birth, ocupation, about to admin panel for personalee

--- slatino/apps/Personalee/admin.py
+++ slatino/apps/Personalee/admin.py
@@ -1,11 +1,11 @@
 from slatino.apps.Personalee.models import Personalee
 from django.contrib import admin
 
-#class PersonaleeAdmin(admin.ModelAdmin):
-#    list_display=('id', 'first_name', 'phone', '', 'date')
-#
-#    class Media:
-#        js = ('/tiny_mce/tiny_mce.js',
-#              '/tiny_mce/textarea.js',
-#             )
-admin.site.register(Personalee)
+class PersonaleeAdmin(admin.ModelAdmin):
+    list_display=('id', 'first_name', 'date_of_birth', 'occupation', 'phone', 'about')
+
+    class Media:
+        js = ('/tiny_mce/tiny_mce.js',
+              '/tiny_mce/textarea.js',
+             )
+admin.site.register(Personalee, PersonaleeAdmin)
