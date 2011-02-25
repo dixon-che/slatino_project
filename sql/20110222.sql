8b05ba2b8197069b8faafb392bd8b67db52adcf7
Lextor <lextor@ukr.net>
2011-02-25 12:19:05 +0200

    fix transport links in menu

--- sql/20110222.sql
+++ sql/20110222.sql
@@ -1,3 +1,5 @@
 alter table Personalee_personalee rename to Persons_person;
 alter table Personalee_personalee_tags rename to Persons_person_tags;
 alter table Persons_person_tags change column personalee_id person_id integer NOT NULL;
+alter table Telephone_phone change column personalee_id person_id integer NOT NULL;
+alter table Institute_occupationperiod change column personalee_id person_id integer NOT NULL;
